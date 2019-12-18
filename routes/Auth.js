a0b344607c6aeabef3289eaca887beb7a46abafd
Riadh <riadh.bouchahoua@estiam.com>
2019-12-18 19:10:08 +0100

    quick edit

--- routes/Auth.js
+++ routes/Auth.js
@@ -5,21 +5,17 @@ const bcrypt = require("bcryptjs");
 const Joi = require("@hapi/joi");
 const User = require("../models/User")
 
-
-
 //TODO
 
 const httpSchemaValidation = (input , type) => {
     let schema;
     if(type === "login") {
-  
         schema = {
                 email : Joi.string().min(5).required(),
                 password : Joi.string().min(5).required(),
                 }
         }
     else if (type === "register") {
-     
             schema = {
                 email : Joi.string().min(5).required(),
                 password : Joi.string().min(5).required(),
@@ -28,7 +24,6 @@ const httpSchemaValidation = (input , type) => {
                 education : Joi.string().required()
             }
         }
- 
         return Joi.validate(input, schema)
 } 
 
@@ -36,9 +31,7 @@ auth.post('/register', async (req, res) =>{
     const {email , password, firstName, lastName, education} = req.body
     const {error} = httpSchemaValidation(req.body , "register") 
     if (error) return res.status(400).send(error.details[0].message)
-    
     const today = new Date()
-
     const userData = {
         email : email,
         password : password,
@@ -47,73 +40,56 @@ auth.post('/register', async (req, res) =>{
         education : education,
         created_at : today
     }
-
     // Verification si l'utilisateur existe deja ?
-
     try {
         const userIsFound = await User.findOne({email : userData.email})
         if (userIsFound) return res.status(409).json({err : "Mail alerady registred !"})
-    }catch(err)
-    {
+    } catch(err) { 
         return res.status(500).json({err})
-    }
-
+        }
     // Cryptage du password
-
     const hashedpassword = await bcrypt.hash(password , 10)
     userData.password = hashedpassword
-
     // Enregistrement de l'utilisateur
-    try{
+    try {
         await User.create(userData)
         return res.status(201).json({msg : "User successfully created !"})
-    }catch(err){
+    } catch(err) {
         return res.status(500).json({err})
     }
-
 })
-
 auth.post('/login', async (req, res) =>{
-
     const {email , password} = req.body
     const {error} = httpSchemaValidation(req.body,"login") 
     if (error) return res.status(400).send(error.details[0].message)
-   
     // Verification exist ?
     const userFound = await User.findOne({
         email
     })
     if (!userFound) return res.status(401).json({err : "User or password incorrect"})
-
-    // verification du mot de passe
+    // Vérification du mot de passe
     const passwordMatch = bcrypt.compareSync(password, userFound.password)
-
     // creation de token JWT
-
     let payload = {
-        id : userFound._id , 
-        email : userFound.email,
+        id: userFound._id , 
+        email: userFound.email,
         firstName: userFound.firstName,
         lastname: userFound.lastName,
         education: userFound.education
     }
     let token = jwt.sign(payload, process.env.SECRET_KEY , {
-        expiresIn: 7200
-        //2hours
+        expiresIn: 7200 //2hours
     })
-
-    if(passwordMatch) {
-
+    if (passwordMatch) {
     return res.status(200).json({token , user : {
-        id : userFound._id , 
-        email : userFound.email , 
-        created : userFound.created_at
+        id: userFound._id , 
+        email: userFound.email , 
+        created: userFound.created_at
     }})
-    }else {
+    } else {
         return res.status(401).json({err : "User or password incorrect"})
-    }
+        }
 })
-
 
 module.exports = auth
 
--- routes/CvEditing.js
+++ routes/CvEditing.js
@@ -4,16 +4,18 @@ const Joi = require("@hapi/joi");
 const authroute = require('../middlewares/authmiddleware')
 
 
-cv_edit.post("/new" , authroute ,(req,res) => {
+cv_edit.post("/new" , authroute , async (req,res) => {
     //on choppe le token on le dechiffre ,on recupere l'id de la personne faisant la requete (req.user._id)
     //on crer un nvx modele de cv dans la db &&
     //on edit le Modele USer et on lui ajoute l'id du cv a sa liste  creation_CV de USER
+    //on lui retourne l'id du cv ( react routage dynamique )
     return
 })
 
 
-cv_edit.get('/get_cv',authroute,(req,res) => {
-    // ici on renvoi le status de user sans le token !
+cv_edit.get('/get_cv/:id',authroute, async (req,res) => {
+    // ici on renvoi le status de user dans le token !
+    // on verifie si l'id du cv fait parti de l'utilisateur
     // sert a recuperer la propriété creation_CV , 
     // appelé a chaque visite sur le dashboard ! --> stored inside localstorage
     // sert de page d'actualisation
@@ -21,6 +23,12 @@ cv_edit.get('/get_cv',authroute,(req,res) => {
 })
 
 
+cv_edit.put('/save_cv/:id' , async (req,res) => {
+    // l'utilisateur nous envoi son state avec les info du cv + l'id du cv
+    // on va modifier le cv en question (id) avec la nouvelle data
+    // on retourne a l'user la data saved
 
+})
+ 
 
 module.exports = cv_edit
